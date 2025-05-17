1cc161f8d7eef05c7421c75339750942d04ce13c
kintadsvn <kintaaaw@gmail.com>
2025-05-17 08:08:45 +0700

    Responsive landing page and about supplier plus page

---
+++ resources/views/components/partials/power.blade.php
@@ -0,0 +1,77 @@
+<section class="relative px-6 py-6 sm:py-16 sm:px-16 bg-[#003A3A] md:bg-transparent">
+    <img class="absolute inset-0 w-full h-auto hidden md:block" src="{{ asset('image/power-bg.png')}}">
+    <div class="flex flex-col items-center gap-10 container relative">
+        <div class="text-center">
+            <p class="text-[#F9FEFE] text-3xl sm:text-5xl font-bold leading-[150%]">
+                Keunggulan Fitur Supplier+
+            </p>
+        </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+            <!-- Power Card 1 -->
+            <div class="bg-white p-6 rounded-lg space-y-3 flex flex-col h-full justify-between">
+                <img src="{{ asset('') }}" class="w-12 h-12" alt="">
+                <h4 class="text-[#003C43] text-xl font-bold">Lorem Ipsum</h4>
+                <p class="text-gray-500 text-base md:mt-4">
+                    Yes! Growkm provides smart tools and resources to help you grow personally
+                    and professionally. From goal setting to progress tracking, we make
+                    self-improvement easier and more effective.
+                </p>
+            </div>
+
+            <!-- Power Card 2 -->
+            <div class="bg-white p-6 rounded-lg space-y-3 flex flex-col h-full justify-between hidden sm:block">
+                <img src="{{ asset('') }}" class="w-12 h-12" alt="">
+                <h4 class="text-[#003C43] text-xl font-bold">Lorem Ipsum</h4>
+                <p class="text-gray-500 text-base md:mt-4">
+                    Yes! Growkm provides smart tools and resources to help you grow personally
+                    and professionally. From goal setting to progress tracking, we make
+                    self-improvement easier and more effective.
+                </p>
+            </div>
+
+            <!-- Power Card 3 -->
+            <div class="bg-white p-6 rounded-lg space-y-3 flex flex-col h-full justify-between hidden sm:block">
+                <img src="{{ asset('') }}" class="w-12 h-12" alt="">
+                <h4 class="text-[#003C43] text-xl font-bold">Lorem Ipsum</h4>
+                <p class="text-gray-500 text-base md:mt-4">
+                    Yes! Growkm provides smart tools and resources to help you grow personally
+                    and professionally. From goal setting to progress tracking, we make
+                    self-improvement easier and more effective.
+                </p>
+            </div>
+
+            <!-- Power Card 4 -->
+            <div class="bg-white p-6 rounded-lg space-y-3 flex flex-col h-full justify-between hidden sm:block">
+                <img src="{{ asset('') }}" class="w-12 h-12" alt="">
+                <h4 class="text-[#003C43] text-xl font-bold">Lorem Ipsum</h4>
+                <p class="text-gray-500 text-base md:mt-4">
+                    Yes! Growkm provides smart tools and resources to help you grow personally
+                    and professionally. From goal setting to progress tracking, we make
+                    self-improvement easier and more effective.
+                </p>
+            </div>
+
+            <!-- Power Card 5 -->
+            <div class="bg-white p-6 rounded-lg space-y-3 flex flex-col h-full justify-between hidden sm:block">
+                <img src="{{ asset('') }}" class="w-12 h-12" alt="">
+                <h4 class="text-[#003C43] text-xl font-bold">Lorem Ipsum</h4>
+                <p class="text-gray-500 text-base md:mt-4">
+                    Yes! Growkm provides smart tools and resources to help you grow personally
+                    and professionally. From goal setting to progress tracking, we make
+                    self-improvement easier and more effective.
+                </p>
+            </div>
+
+            <!-- Power Card 6 -->
+            <div class="bg-white p-6 rounded-lg space-y-3 flex flex-col h-full justify-between hidden sm:block">
+                <img src="{{ asset('') }}" class="w-12 h-12" alt="">
+                <h4 class="text-[#003C43] text-xl font-bold">Lorem Ipsum</h4>
+                <p class="text-gray-500 text-base md:mt-4">
+                    Yes! Growkm provides smart tools and resources to help you grow personally
+                    and professionally. From goal setting to progress tracking, we make
+                    self-improvement easier and more effective.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/landingpage/about-supplier-plus.blade.php
+++ resources/views/landingpage/about-supplier-plus.blade.php
@@ -3,4 +3,9 @@
     <x-partials.header></x-partials.header>
 
     {{-- Make content bellow! --}}
+    <x-partials.hero-main></x-partials.hero-main>
+    <x-partials.favourite-feature></x-partials.favourite-feature>
+    <x-partials.power></x-partials.power>
+    <x-partials.pricing></x-partials.pricing>
+    <x-partials.faq-contact></x-partials.faq-contact>
 </x-layouts.client-side-layout>
