bd24155a7ce603eebc0e5897f8a1721102115515
Rafly Firmansyah <raflyfirmansyah017@gmail.com>
2025-06-04 15:09:10 +0700

    feat: add schema database users, events, settings

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,28 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->id('user_id')->primary();
+            $table->string("user_name", 100);
+            $table->string("email", 100)->unique();
+            $table->string("password", 255);
+            $table->string("user_gender", 50);
+            $table->string("user_phone", 15)->unique();
+            $table->string("user_address", 255)->nullable();
+            $table->string("user_profile", 255)->nullable();
+            $table->string("user_role", 50);
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id('role_id', 10)->primary();
+            $table->string('role_name', 50);
+            $table->timestamps();
         });
     }
 
@@ -43,7 +37,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('roles');
     }
 };
