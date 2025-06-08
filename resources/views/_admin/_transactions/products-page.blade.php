d14203d2d6e074c317b5c1396ebef10505b5f50e
Rafly Firmansyah <raflyfirmansyah017@gmail.com>
2025-06-08 09:06:38 +0700

    feat: responsive table for event & porudct transaction page

--- resources/views/_admin/_transactions/products-page.blade.php
+++ resources/views/_admin/_transactions/products-page.blade.php
@@ -3,10 +3,11 @@
 
 
 
-    <div class="p-6 bg-white w-full rounded-t-md flex items-center justify-between border-b border-gray-200">
-        <h1 class="font-semibold text-lg">Data Transaksi Product</h1>
+    <div
+        class="p-6 bg-white w-full gap-8 rounded-t-md flex lg:flex-row flex-col items-center justify-between border-b border-gray-200">
+        <h1 class="font-semibold text-lg w-auto text-center lg:text-start">Data Transaksi Product</h1>
 
-        <div class="flex items-center gap-4">
+        <div class="flex flex-col md:flex-row items-center gap-4 w-full lg:w-auto justify-between lg:justify-end">
 
             <form action="#" method="get">
                 @csrf
@@ -18,122 +19,122 @@
                 </div>
             </form>
 
-            <div x-data="{ open: false }" class="relative inline-block text-left">
-                <div>
-                    <button type="button" @click="open = !open"
-                        class="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-md font-semibold text-gray-800 border border-gray-200 hover:bg-gray-50"
-                        id="menu-button" aria-expanded="true" aria-haspopup="true">
-                        Pesanan Masuk
-                        <x-icons.arrow-down class="stroke-dark-base size-5"></x-icons.arrow-down>
-                    </button>
-                </div>
+            <div>
+                <div x-data="{ open: false }" class="relative inline-block text-left">
+                    <div>
+                        <button type="button" @click="open = !open"
+                            class="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-md font-semibold text-gray-800 border border-gray-200 hover:bg-gray-50"
+                            id="menu-button" aria-expanded="true" aria-haspopup="true">
+                            Pesanan Masuk
+                            <x-icons.arrow-down class="stroke-dark-base size-5"></x-icons.arrow-down>
+                        </button>
+                    </div>
 
-                <div x-show="open" @click.away="open = false" x-transition:enter="transition ease-out duration-100"
-                    x-transition:enter-start="transform opacity-0 scale-95"
-                    x-transition:enter-end="transform opacity-100 scale-100"
-                    x-transition:leave="transition ease-in duration-75"
-                    x-transition:leave-start="transform opacity-100 scale-100"
-                    x-transition:leave-end="transform opacity-0 scale-95"
-                    class="absolute right-0 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
-                    role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
-                    style="display: none;">
-                    <div class="py-1" role="none">
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-1">Semua Status</a>
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-1">Pesanan Masuk</a>
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-2">Pesanan Diprosess</a>
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-3">Selesai</a>
+                    <div x-show="open" @click.away="open = false" x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+                        role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
+                        style="display: none;">
+                        <div class="py-1" role="none">
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-1">Semua Status</a>
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-1">Pesanan Masuk</a>
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-2">Pesanan Diprosess</a>
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-3">Selesai</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div x-data="{ open: false }" class="relative inline-block text-left">
-                <div>
-                    <button type="button" @click="open = !open"
-                        class="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-md font-semibold text-gray-800 border border-gray-200 hover:bg-gray-50"
-                        id="menu-button" aria-expanded="true" aria-haspopup="true">
-                        Hari ini
-                        <x-icons.arrow-down class="stroke-dark-base size-5"></x-icons.arrow-down>
-                    </button>
-                </div>
+                <div x-data="{ open: false }" class="relative inline-block text-left">
+                    <div>
+                        <button type="button" @click="open = !open"
+                            class="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-md font-semibold text-gray-800 border border-gray-200 hover:bg-gray-50"
+                            id="menu-button" aria-expanded="true" aria-haspopup="true">
+                            Hari ini
+                            <x-icons.arrow-down class="stroke-dark-base size-5"></x-icons.arrow-down>
+                        </button>
+                    </div>
 
-                <div x-show="open" @click.away="open = false" x-transition:enter="transition ease-out duration-100"
-                    x-transition:enter-start="transform opacity-0 scale-95"
-                    x-transition:enter-end="transform opacity-100 scale-100"
-                    x-transition:leave="transition ease-in duration-75"
-                    x-transition:leave-start="transform opacity-100 scale-100"
-                    x-transition:leave-end="transform opacity-0 scale-95"
-                    class="absolute right-0 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
-                    role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
-                    style="display: none;">
-                    <div class="py-1" role="none">
+                    <div x-show="open" @click.away="open = false" x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+                        role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
+                        style="display: none;">
+                        <div class="py-1" role="none">
 
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-1">Hari ini</a>
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-1">Mingguan</a>
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-2">Bulanan</a>
-                        <a href="#"
-                            class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
-                            role="menuitem" tabindex="-1" id="menu-item-3">Tahunan</a>
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-1">Hari ini</a>
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-1">Mingguan</a>
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-2">Bulanan</a>
+                            <a href="#"
+                                class="block px-4 py-3 text-md text-gray-700 hover:bg-gray-50 hover:text-gray-800"
+                                role="menuitem" tabindex="-1" id="menu-item-3">Tahunan</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <table class="w-full bg-white text-left">
-        <thead class="text-sm uppercase bg-gray-50">
-            <tr>
-                <th class="py-4 px-2 w-12 h-12 text-center">#</th>
-                <th class="py-4 px-2">Transaction ID</th>
-                <th class="py-4 px-2">Created At</th>
-                <th class="py-4 px-2">Nama Pelanggan</th>
-                <th class="py-4 px-2">Deskripsi Pesanan</th>
-                <th class="py-4 px-2">Total Payment</th>
-                <th class="py-4 px-2 text-center">Payment status</th>
-                <th class="text-center p-4">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @for ($i = 0; $i < 10; $i++)
-                <tr class="border-b border-gray-200">
-                    <td class="text-center">{{ $i + 1 }}</td>
-                    <td class="py-4 px-2">{{ '#TRXPRT00' . $i + 1 }}</td>
-                    <td class="py-4 px-2">07/06/25 - 22:35</td>
-                    <td class="py-4 px-2">Jhon dae</td>
-                    <td class="py-4 px-2">Bisnis Digital with Growkm</td>
-                    <td class="py-4 px-2 text-center">Rp. 0</td>
-                    <td class="py-4 px-2 text-center">Pesanan Masuk</td>
-                    <td class=" w-full py-4  flex items-center justify-center gap-2">
-                        <a href="{{ url('#') }}"
-                            class="bg-secondaryColors-10 flex items-center justify-center w-8 h-8 rounded-md hover:bg-secondaryColors-20">
-                            <x-icons.eye-01 class="size-5 stroke-secondaryColors-base "></x-icons.eye-01>
-                        </a>
-                        <a href="{{ url('#') }}"
-                            class="bg-orange-100 flex items-center justify-center w-8 h-8 rounded-md hover:bg-secondaryColors-20">
-                            <x-icons.truck-shipping class="size-5 stroke-orange-600 "></x-icons.truck-shipping>
-                        </a>
-                        <a href="{{ url('#') }}"
-                            class="bg-red-100 flex items-center justify-center w-8 h-8 rounded-md hover:bg-secondaryColors-20">
-                            <x-icons.delete-01 class="size-5 stroke-red-600 "></x-icons.delete-01>
-                        </a>
-                    </td>
+    <div class="overflow-x-auto w-full">
+        <table class="w-full bg-white text-left min-w-[800px]">
+            <thead class="text-sm uppercase bg-gray-50">
+                <tr>
+                    <th class="py-4 px-2 w-12 h-12 text-center">#</th>
+                    <th class="py-4 px-2">Transaction ID</th>
+                    <th class="py-4 px-2">Created At</th>
+                    <th class="py-4 px-2">Nama Pelanggan</th>
+                    <th class="py-4 px-2">Deskripsi Pesanan</th>
+                    <th class="py-4 px-2">Total Payment</th>
+                    <th class="py-4 px-2 text-center">Payment status</th>
+                    <th class="text-center p-4">Action</th>
                 </tr>
-            @endfor
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @for ($i = 0; $i < 10; $i++)
+                    <tr class="border-b border-gray-200">
+                        <td class="text-center">{{ $i + 1 }}</td>
+                        <td class="py-4 px-2">{{ '#TRXPRT00' . $i + 1 }}</td>
+                        <td class="py-4 px-2">07/06/25 - 22:35</td>
+                        <td class="py-4 px-2">Jhon dae</td>
+                        <td class="py-4 px-2">Bisnis Digital with Growkm</td>
+                        <td class="py-4 px-2 text-center">Rp. 0</td>
+                        <td class="py-4 px-2 text-center">Pesanan Masuk</td>
+                        <td class="w-full py-4 flex items-center justify-center gap-2 px-6">
+                            <a href="{{ url('#') }}"
+                                class="bg-secondaryColors-10 flex items-center justify-center w-8 h-8 rounded-md hover:bg-secondaryColors-20">
+                                <x-icons.eye-01 class="size-5 stroke-secondaryColors-base "></x-icons.eye-01>
+                            </a>
+                            <a href="{{ url('#') }}"
+                                class="bg-orange-100 flex items-center justify-center w-8 h-8 rounded-md hover:bg-orange-200">
+                                <x-icons.truck-shipping class="size-5 stroke-orange-600 "></x-icons.truck-shipping>
+                            </a>
+                        </td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
     <div class="p-6 bg-white w-full rounded-b-lg flex items-center justify-between">
         <span>Showing 1 to 10 of 57 entries</span>
 
